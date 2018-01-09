8cb93226a21877a5cf80120a7e7f82555ed697ec
sramos9 <ramos.samantha9@gmail.com>
2018-01-09 15:21:17 -0600

    object practice

--- old_homework/app.js
+++ old_homework/app.js
@@ -1,1 +1,33 @@
-console.log('day one... setup');
+// console.log('day one... setup');
+//
+// const orphan = {}
+//
+// const dog = {
+//   name: "Gizmo",
+//   age: 1,
+//   breed: "doodle"
+// }
+//
+// console.log(dog);
+// console.log(dog.name);
+// console.log(dog.age);
+// console.log(dog.breed);
+
+const celebrity = {
+  name: "Trump",
+  age: 55,
+  isCurrentlyTweeting: true
+}
+
+console.log(celebrity);
+console.log(celebrity.name);
+console.log(celebrity.age);
+console.log(celebrity.isCurrentlyTweeting);
+
+const Twitter = ()=> {
+  if(celebrity.isCurrentlyTweeting === true){
+    console.log('Turn off Twitter');
+  }
+}
+
+Twitter();
