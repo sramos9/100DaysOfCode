9c18d9e2d8e0aedf9e769a3f212a4b262683a529
sramos9 <ramos.samantha9@gmail.com>
2018-01-10 10:55:29 -0600

    day 2 objects

--- old_homework/app.js
+++ old_homework/app.js
@@ -13,21 +13,74 @@
 // console.log(dog.age);
 // console.log(dog.breed);
 
-const celebrity = {
-  name: "Trump",
-  age: 55,
-  isCurrentlyTweeting: true
-}
+// const celebrity = {
+//   name: "Trump",
+//   age: 55,
+//   isCurrentlyTweeting: true
+// }
+//
+// console.log(celebrity);
+// console.log(celebrity.name);
+// console.log(celebrity.age);
+// console.log(celebrity.isCurrentlyTweeting);
+//
+// const Twitter = ()=> {
+//   if(celebrity.isCurrentlyTweeting === true){
+//     console.log('Turn off Twitter');
+//   }
+// }
+//
+// Twitter();
 
-console.log(celebrity);
-console.log(celebrity.name);
-console.log(celebrity.age);
-console.log(celebrity.isCurrentlyTweeting);
+// Adding properties
 
-const Twitter = ()=> {
-  if(celebrity.isCurrentlyTweeting === true){
-    console.log('Turn off Twitter');
-  }
-}
+// const house = {
+//   doors: 9,
+//   windows: 20,
+//   hasGarden: true
+// }
+//
+// console.log(house);
 
-Twitter();
+
+
+// Changing properties //
+
+// const bicycle = {
+//   isATricycle: false
+// }
+//
+// bicycle.isATricycle = true
+//
+// console.log(bicycle.isATricycle);
+
+// const macros = {}
+//
+// macros.protein = "tempeh"
+// macros.carbohydrates = "spuds"
+// macros.fats = "olive oil"
+//
+// console.log(macros);
+
+// const guitar = {
+//   strings: 6,
+//   isAcoustic: true
+// }
+//
+// guitar.strings = 100
+// guitar.isAcoustic = false
+// console.log(guitar.isAcoustic);
+//
+// guitar.belongsTo = "Dimebag Darrell"
+// console.log(guitar);
+//
+// guitar.belongsTo = "Mr. Rogers"
+// console.log(guitar);
+
+// Object literal shorthand
+
+const name = "gizmo";
+const age = 1;
+
+const mogwai = {name, age}
+console.log(mogwai);
