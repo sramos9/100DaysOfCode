f263255bcfd4dde6895cc250786bac034dc21355
sramos9 <ramos.samantha9@gmail.com>
2018-01-23 13:11:53 -0600

    blog setup

--- blog_ex/controllers/authors.js
+++ blog_ex/controllers/authors.js
@@ -1,8 +1,61 @@
 const express = require('express');
-const router = express.router();
+const router = express.Router();
+const Author = require('../models/authors.js');
+
+
+
+router.get('/', (req, res)=>{
+	Author.find({}, (err, foundAuthors)=>{
+		res.render('authors/index.ejs', {
+			authors: foundAuthors
+		});
+	})
+});
+
+router.get('/', (req, res) => {
+  res.render('authors/new.ejs');
+});
 
 router.get('/', (req, res) => {
   res.render('authors/index.ejs');
+});
+
+
+
+
+router.post('/', (req, res) =>{
+  Author.create(req.body, (err, createdAuthor)=> {
+    res.redirect('/authors');
+  });
+});
+
+router.get('/:id/edit', (req, res)=>{
+	Author.findById(req.params.id, (err, foundAuthor)=>{
+		res.render('authors/edit.ejs', {
+			author: foundAuthor
+		});
+	});
+});
+
+router.put('/:id', (req, res)=>{
+	Author.findByIdAndUpdate(req.params.id, req.body, ()=>{
+		res.redirect('/authors');
+	});
+});
+
+router.delete('/:id', (req, res)=>{
+	Author.findByIdAndRemove(req.params.id, ()=>{
+		res.redirect('/authors');
+	});
+});
+
+//avoid this handling /new by placing it towards the bottom of the file
+router.get('/:id', (req, res)=>{
+	Author.findById(req.params.id, (err, foundAuthor)=>{
+		res.render('authors/show.ejs', {
+			author: foundAuthor
+		});
+	});
 });
 
 module.exports = router;
--- blog_ex/server.js
+++ blog_ex/server.js
@@ -1,13 +1,24 @@
 const express = require('express');
 const app = express();
-const authorsController = require('./controllers/author.js');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override')
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(methodOverride('_method'));
+
+
+const authorsController = require('./controllers/authors.js');
 app.use('/authors', authorsController);
 
 app.get('/', (req, res) => {
   res.render('index.ejs');
 });
 
-
+mongoose.connect('mongodb://localhost:27017/blog');
+mongoose.connection.once('open', () =>{
+  console.log('connected to mongo');
+});
 
 app.listen(3000, ()=>{
   console.log('litening on port 3000...');
