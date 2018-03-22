46d49beeeda0daacbf666ed8efb841cefef9682f
sramos9 <ramos.samantha9@gmail.com>
2018-03-22 12:28:49 -0500

    js obj

--- udemy_js/js/app.js
+++ udemy_js/js/app.js
@@ -10,3 +10,16 @@ var question = true;
 if (question == true) {
   console.log("Is true");
 }
+
+//var movies = ['Die Hard', 'Avatar', 'Forrest Gump'];
+//console.log(movies);
+//console.log(movies[1]);
+
+//var array = [1, 2, 3, 4];
+//console.log(array[0]);
+
+// var user = {
+//   name: "James",
+//   lastName: "Jones"
+// };
+//console.log(user.name);
