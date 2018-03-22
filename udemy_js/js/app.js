e43684dc657534ce2ad74b5fa22c1b525a9efbea
sramos9 <ramos.samantha9@gmail.com>
2018-03-22 12:12:13 -0500

    js basics

---
+++ udemy_js/js/app.js
@@ -0,0 +1,12 @@
+console.log('run import');
+
+var myFirstName = 'Sam';
+
+console.log(myFirstName);
+
+var number = 1;
+var question = true;
+
+if (question == true) {
+  console.log("Is true");
+}
