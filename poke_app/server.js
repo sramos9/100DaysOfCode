63c2f6574bc35b42dc552e283fe91cbaa430060c
sramos9 <ramos.samantha9@gmail.com>
2018-05-04 13:36:21 -0500

    Commit 4 - Connected my database, can see json in the browser

--- poke_app/server.js
+++ poke_app/server.js
@@ -6,6 +6,11 @@ app.get('/', (req, res) => {
   res.send('Welcom to the Pokemon App!');
 });
 
+app.get('/pokemon', (req, res) => {
+  res.render('index.ejs', {
+    pokemon: pokemon
+  });
+});
 
 app.listen(3000, ()=> {
   console.log('listening');
